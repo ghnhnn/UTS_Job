cdfc74cd45a910d03bc284f524b85cce159a29d0
GhinaaHaniifah <2010817120010@mhs.ulm.ac.id>
2022-04-08 12:08:59 +0800

    -

--- app/src/main/java/com/example/job/data/Datasource.kt
+++ app/src/main/java/com/example/job/data/Datasource.kt
@@ -8,15 +8,15 @@ class Datasource {
     fun loadJob(): List<Job> {
         return listOf<Job>(
             Job(R.string.job1, R.drawable.image_1, R.string.desc1),
-            Job(R.string.job2, R.drawable.image_2, R.string.desc1),
-            Job(R.string.job3, R.drawable.image_3, R.string.desc1),
-            Job(R.string.job4, R.drawable.image_4, R.string.desc1),
-            Job(R.string.job5, R.drawable.image_5, R.string.desc1),
-            Job(R.string.job6, R.drawable.image_6, R.string.desc1),
-            Job(R.string.job7, R.drawable.image_7, R.string.desc1),
-            Job(R.string.job8, R.drawable.image_8, R.string.desc1),
-            Job(R.string.job9, R.drawable.image_9, R.string.desc1),
-            Job(R.string.job10, R.drawable.image_10, R.string.desc1)
+            Job(R.string.job2, R.drawable.image_2, R.string.desc2),
+            Job(R.string.job3, R.drawable.image_3, R.string.desc3),
+            Job(R.string.job4, R.drawable.image_4, R.string.desc4),
+            Job(R.string.job5, R.drawable.image_5, R.string.desc5),
+            Job(R.string.job6, R.drawable.image_6, R.string.desc6),
+            Job(R.string.job7, R.drawable.image_7, R.string.desc7),
+            Job(R.string.job8, R.drawable.image_8, R.string.desc8),
+            Job(R.string.job9, R.drawable.image_9, R.string.desc9),
+            Job(R.string.job10, R.drawable.image_10, R.string.desc10)
         )
     }
 }
